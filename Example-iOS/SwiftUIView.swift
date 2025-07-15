ae132a781adae56254b0a3076e20241a468f9625
WenkangLi <wenkang.li@shanbay.com>
2025-07-15 17:35:54 +0800

    ref: set minimum deployment target to iOS 13

---
+++ Example-iOS/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  Example-iOS
+//
+//  Created by 李文康 on 2025/7/15.
+//  Copyright © 2025 Shanbay iOS. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
